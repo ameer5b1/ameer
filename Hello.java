e6a5019862d396c95089c844df8a167ad409ce36
ameer5b1 <ameer7672013@gmail.com>
2025-07-30 12:30:22 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,5 @@
+public class Hello { // Class declaration
+    public static void main(String[] args) { // Main method, entry point of execution
+        System.out.println("Hell World!"); // Statement to print output
+    }
+}
